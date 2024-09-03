ef3f6cc1a71d6f39f827e06685c62560a9d0f1c0
Bolo New <121732191+Bol-o@users.noreply.github.com>
2024-09-03 06:20:10 +0100

    long time since touched this...

--- moreTesting.js
+++ moreTesting.js
@@ -1,9 +1,22 @@
-var string = 89;
-var dog = 'dog'
-console.log(typeof(string))
-var notString = 12345.46;
-string = notString.toFixed(1)
-//string = parseFloat(string)
-console.log(typeof(notString))
-console.log(typeof(string))
-console.log(string+dog)
+function Book(title, author, numbPages){
+    this.title = title;
+    this.author = author;
+    this.numbPages = numbPages;
+    // this.info = function(){
+    //     return title + ' '+ author+ ', '+ numbPages + ' pages'+ ', ' + 'not read yet'
+    // }
+};
+
+const theHobbit = new Book('The Hobbit by J.R.R.', 'Tolkien', '295' );
+console.log(theHobbit.title);
+// console.log(Object.getPrototypeOf(Book));
+// console.log(Object.getPrototypeOf(theHobbit) == Book.prototype)
+function Player(){};
+Player.prototype.sayHello = function() {
+    console.log("Hello, I'm a player!");
+ };
+ const player1 = new Player();
+ const player2 = new Player();
+ player1.sayHello(); // logs "Hello, I'm a player!"
+ player2.sayHello(); // logs "Hello, I'm a player!"
+ console.log(theHobbit.valueOf());
---
+++ testingMore.js
@@ -0,0 +1,15 @@
+// const testing = function(){
+
+//     for(let i = 0; i<4; i++){
+//         console.log('one')
+//     }
+
+//   const testing2 = function(){  
+//     console.log('two')}
+//     setTimeout(testing2, 0)
+//     console.log('three')
+// }
+
+// testing()
+process.env.USER_ID; // "239482"
+process.env.USER_KEY; // "foobar"
